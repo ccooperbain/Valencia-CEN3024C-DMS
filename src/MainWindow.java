d212beef857f5c2b39c0c1c72c751e592c8aa4f8
Christopher Bain <christopher_cb@outlook.com>
2026-03-29 01:28:38 -0400

    Gui commit addition 1.5

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -4,6 +4,7 @@ import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JSpinner;
 
 //TODO merge the month day and year in the colum
 public class MainWindow extends JFrame {
@@ -12,9 +13,13 @@ public class MainWindow extends JFrame {
     private JPanel leftPanel;
     private JPanel centerPanel;
     private JTextField titleEntryField;
+    SpinnerNumberModel monthModel = new SpinnerNumberModel(1,1,31,1);
     private JSpinner monthEntry;
+    SpinnerNumberModel dateModel = new SpinnerNumberModel(1,1,31,1);
     private JSpinner dateEntry;
+    SpinnerNumberModel yearModel = new SpinnerNumberModel(2000,1928,3005,1);
     private JSpinner yearEntry;
+
     private JSlider ratingEntrySlider;
     private JCheckBox isAnimated;
     private JCheckBox onWatchList;
@@ -28,8 +33,12 @@ public class MainWindow extends JFrame {
     private JButton addShowButton1;
     private JButton addShowFromFileButton;
     private JTable tvShowTable;
-    private DefaultTableModel tvShowModel = (DefaultTableModel) tvShowTable.getModel();
-
+    DefaultTableModel tableModel = new DefaultTableModel(new Object[]{"ID","Title","Animated","Month","Date","Year","Rating","Is On Watchlist"}, 0){
+        @Override
+        public boolean isCellEditable(int row, int column) {return false;}
+    };
+    //private DefaultTableModel tvShowModel = (DefaultTableModel) tvShowTable.getModel();
+    private DefaultTableModel tvShowModel = tableModel;
     private JScrollPane scrollPane;
     private JLabel averageResult;
     private JLabel testValue;
@@ -37,7 +46,11 @@ public class MainWindow extends JFrame {
 
 
     public MainWindow(){
-        addColumns();
+        //addColumns();
+        monthEntry.setModel(monthModel);
+        dateEntry.setModel(dateModel);
+        yearEntry.setModel(yearModel);
+        //tvShowTable.setModel(tableModel);
 
 
         addShowButton1.addActionListener(new ActionListener() {
@@ -198,7 +211,7 @@ public class MainWindow extends JFrame {
     }
 
 
-    private void addColumns(){
+    /*private void addColumns(){
         tvShowModel.addColumn("ID");
         tvShowModel.addColumn("Title");
         tvShowModel.addColumn("Is Animated");
@@ -207,5 +220,5 @@ public class MainWindow extends JFrame {
         tvShowModel.addColumn("Year Completed");
         tvShowModel.addColumn("Rating");
         tvShowModel.addColumn("Is On watchlist");
-    }
+    }*/
 }
