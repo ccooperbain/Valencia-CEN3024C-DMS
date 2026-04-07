e9eac6dde1433bb698e426fcbf6f719ff5306775
Christopher Bain <christopher_cb@outlook.com>
2026-04-06 22:11:00 -0400

    Update to detail comments to submit and prepare for Java Docs

---
+++ Docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"DatabaseGui"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"MainWindow"},{"p":"<Unnamed>","l":"TvShow"},{"p":"<Unnamed>","l":"TvShowManager"}];updateSearchResults();
